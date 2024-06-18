491be6fd2c66653840c9ced409449f5305aa2793
Jun <j.yonemura.a@gmail.com>
2024-06-18 17:41:37 +0900

    themes、検索モーダル作成

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ $(document).ready(function() {
         $(this).toggleClass('active');
         $(".header__nav").toggleClass('active');
     });
+    
+    //テーマ検索モーダル
+    $(".trip-themes__search-btn").click(function () {
+        $(".search__modal").toggleClass('active');
+
+        //検索ボタンに重ねてモーダルを表示する場合に使えそう
+        // $(".trip-themes__title").toggleClass('active');
+        // $(".trip-themes__search-btn").toggleClass('active');
+    });
+
+    $(".search__close-btn").click(function () {
+        $(".search__modal").removeClass('active');
+
+        //検索ボタンに重ねてモーダルを表示する場合に使えそう
+        // $(".trip-themes__title").removeClass('active');
+        // $(".trip-themes__search-btn").removeClass('active');
+    });
 
     new Splide('.splide', {
         type   : 'loop',
