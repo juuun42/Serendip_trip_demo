bd5db94c8b3a82708b31467378cdb26b428cff0d
Jun <j.yonemura.a@gmail.com>
2024-06-20 12:00:39 +0900

    header固定設定

--- script.js
+++ script.js
@@ -3,22 +3,14 @@ $(document).ready(function() {
         $(this).toggleClass('active');
         $(".header__nav").toggleClass('active');
     });
-    
-    //テーマ検索モーダル
+
+    // テーマ検索モーダル
     $(".trip-themes__search-btn").click(function () {
         $(".search__modal").toggleClass('active');
-
-        //検索ボタンに重ねてモーダルを表示する場合に使えそう
-        // $(".trip-themes__title").toggleClass('active');
-        // $(".trip-themes__search-btn").toggleClass('active');
     });
 
     $(".search__close-btn").click(function () {
         $(".search__modal").removeClass('active');
-
-        //検索ボタンに重ねてモーダルを表示する場合に使えそう
-        // $(".trip-themes__title").removeClass('active');
-        // $(".trip-themes__search-btn").removeClass('active');
     });
 
     new Splide('.splide', {
@@ -36,7 +28,18 @@ $(document).ready(function() {
         arrows : false,
         autoplay: true,
         interval: 5000,
-      }).mount();
+    }).mount();
+
+    var header = document.querySelector('.header');
+    var windowHeight = window.innerHeight;
+    var scrollThreshold = windowHeight * 0.4;
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > scrollThreshold) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+    
 });
-
-
